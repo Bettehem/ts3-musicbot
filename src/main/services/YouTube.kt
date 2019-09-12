b9bde8c015405eaa69ece16b7914f1958f3457a4
Bettehem <chris.mustola@gmail.com>
2019-09-12 20:50:20 +0300

    Changed getTitle to include the --no-playlist flag and removed the -s flag as it was redundant.

--- src/main/services/YouTube.kt
+++ src/main/services/YouTube.kt
@@ -4,6 +4,6 @@ import src.main.util.runCommand
 
 class YouTube{
     fun getTitle(videoLink: String): String{
-        return runCommand("youtube-dl --geo-bypass -s -e \"$videoLink\"", printErrors = false)
+        return runCommand("youtube-dl --no-playlist --geo-bypass -e \"$videoLink\" 2> /dev/null", printErrors = false)
     }
 }
