6e64fcd7de77c48015d609ffc41a84af121afb87
Bettehem <chris.mustola@gmail.com>
2023-07-05 15:08:48 +0300

    Fixed tests.

--- src/test/kotlin/ts3_musicbot/SoundCloudTest.kt
+++ src/test/kotlin/ts3_musicbot/SoundCloudTest.kt
@@ -48,7 +48,7 @@ class SoundCloudTest {
             //SoundCloud link to track: i am leeya - something worth dreaming of (leeyas mashup)
             val testLink = Link("https://soundcloud.com/iamleeya/something-worth-dreaming-of")
             val track = soundCloud.fetchTrack(testLink)
-            assertEquals("i am leeya", track.artists.artists[0].name.name)
+            assertEquals("ajnabiyeh", track.artists.artists[0].name.name)
             assertEquals("something worth dreaming of (leeyas mashup)", track.title.name)
         }
     }
