a77a929da50c010f8e5078fa3cea50ed086b7a22
Bettehem <chris.mustola@gmail.com>
2024-06-11 17:40:07 +0300

    Fixed jar buildscript

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 mkdir -p out/artifacts/ts3_musicbot
 # Build using gradle and if successful, copy the .jar file to the output directory.
 ./gradlew assemble "$@" && echo "Copying ts3-musicbot.jar to out/articats/ts3_musicbot/" &&
-  cp build/libs/ts3-musicbot.jar out/artifacts/ts3_musicbot/. && echo "Done."
+  cp app/build/libs/ts3-musicbot.jar out/artifacts/ts3_musicbot/. && echo "Done."
